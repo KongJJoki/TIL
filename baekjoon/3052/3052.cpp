52bb1519e3af41e45196b690e5c9139472ffbe81
KongJJoki <jeonga0208@gmail.com>
2023-10-23 18:15:26 +0900

    나머지

---
+++ baekjoon/3052/3052.cpp
@@ -0,0 +1,43 @@
+﻿#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+	int arrNumberB[10]{};
+	int arrSameRemain[42]{};
+	int nArrayLength{};
+	int nArrayRemainLength{};
+	int nCount{};
+	int nDifCount{};
+
+	nArrayLength = 10;
+	nArrayRemainLength = 42;
+
+	for (int i = 0; i < nArrayLength; i++)
+	{
+		int nTmp{};
+
+		scanf_s("%d", &nTmp);
+		while (nTmp > 1000 || nTmp < 0)
+		{
+			scanf_s("%d", &nTmp);
+		}
+
+		arrNumberB[i] = nTmp % 42;
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		arrSameRemain[arrNumberB[i]]++;
+	}
+
+	for (int i = 0; i < nArrayRemainLength; i++)
+	{
+		if (arrSameRemain[i] != 0)
+		{
+			nDifCount++;
+		}
+	}
+
+	printf("%d", nDifCount);
+}
