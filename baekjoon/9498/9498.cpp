b198efa4a6360e6eeb842f5937b2de20b87b3974
KongJJoki <jeonga0208@gmail.com>
2023-10-23 18:11:36 +0900

    시험성적

---
+++ baekjoon/9498/9498.cpp
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int nScore{};
+	int nDivideScore{};
+	char cScore{};
+
+	cScore = 'F';
+
+	scanf_s("%d", &nScore);
+	while (nScore < 0 || nScore > 100)
+	{
+		scanf_s("%d", &nScore);
+	}
+
+	nDivideScore = nScore / 10;
+
+	if (nDivideScore == 10 || nDivideScore == 9)
+	{
+		cScore = 'A';
+	}
+	else if (nDivideScore == 8)
+	{
+		cScore = 'B';
+	}
+	else if (nDivideScore == 7)
+	{
+		cScore = 'C';
+	}
+	else if (nDivideScore == 6)
+	{
+		cScore = 'D';
+	}
+	printf("%c", cScore);
+}
