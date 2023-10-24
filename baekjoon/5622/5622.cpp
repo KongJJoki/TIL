c7b082a10237ecd254fb51049d664c042121dc08
KongJJoki <jeonga0208@gmail.com>
2023-10-24 21:14:28 +0900

    다이얼

--- baekjoon/5622/5622.cpp
+++ baekjoon/5622/5622.cpp
@@ -1,20 +1,58 @@
-﻿// 5622.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
+﻿#include <stdio.h>
 #include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <stack>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int nTime{};
+	int nStrLength{};
+	std::string strWord{};
+
+	std::cin >> strWord;
+	nStrLength = strWord.length();
+
+	for (int i = 0; i < nStrLength; i++)
+	{
+		char cTmp{};
+		cTmp = strWord[i];
+
+		if (cTmp >= 'A' && cTmp <= 'C')
+		{
+			nTime += 3;
+		}
+		else if (cTmp >= 'D' && cTmp <= 'F')
+		{
+			nTime += 4;
+		}
+		else if (cTmp >= 'G' && cTmp <= 'I')
+		{
+			nTime += 5;
+		}
+		else if (cTmp >= 'J' && cTmp <= 'L')
+		{
+			nTime += 6;
+		}
+		else if (cTmp >= 'M' && cTmp <= 'O')
+		{
+			nTime += 7;
+		}
+		else if (cTmp >= 'P' && cTmp <= 'S')
+		{
+			nTime += 8;
+		}
+		else if (cTmp >= 'T' && cTmp <= 'V')
+		{
+			nTime += 9;
+		}
+		else if (cTmp >= 'W' && cTmp <= 'Z')
+		{
+			nTime += 10;
+		}
+	}
+
+	std::cout << nTime;
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
