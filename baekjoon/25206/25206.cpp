49835b934b3457c7e6015c1a3a89f79b085db3d1
KongJJoki <jeonga0208@gmail.com>
2023-10-25 19:28:09 +0900

    너의 평점은

--- baekjoon/25206/25206.cpp
+++ baekjoon/25206/25206.cpp
@@ -1,20 +1,35 @@
-﻿// 25206.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
-#include <iostream>
+﻿#include <iostream>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <map>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    float fScoreTotal{};
+    float fGradeScoreTotal{};
+    float fResult{};
+
+    std::map<std::string, float> mapGrade{};
+    mapGrade = { {"A+",4.5f},{"A0",4.0f}, {"B+",3.5f}, {"B0",3.0f},
+        {"C+",2.5f}, {"C0",2.0f},{"D+",1.5f},{"D0",1.0f},{"F",0.0f} };
+
+    for (int i = 0; i < 20; i++)
+    {
+        std::string strSubject{};
+        float fTmpScore{};
+        std::string strTmpGrade{};
+
+        std::cin >> strSubject >> fTmpScore >> strTmpGrade;
+
+        if (strTmpGrade != "P")
+        {
+            fScoreTotal += fTmpScore;
+            fGradeScoreTotal += fTmpScore * mapGrade[strTmpGrade];
+        }
+    }
+
+    fResult = fGradeScoreTotal / fScoreTotal;
+
+    std::cout << fResult;
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
