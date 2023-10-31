e5db71c8c2a6b506fb3391b2f0ebaf5e823ae9e9
KongJJoki <jeonga0208@gmail.com>
2023-10-31 13:58:37 +0900

    소인수분해

---
+++ baekjoon/11653/11653.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+int main()
+{
+    int nInputN{};
+    int nRemainder{};
+    int nQuotient{};
+    int nDivider{};
+
+    std::cin >> nInputN;
+
+    nDivider = 2;
+
+    while (nInputN != 1)
+    {
+        nRemainder = nInputN % nDivider;
+        nQuotient = nInputN / nDivider;
+
+        if (nRemainder == 0)
+        {
+            while (nInputN % nDivider == 0)
+            {
+                nInputN /= nDivider;
+                std::cout << nDivider << '\n';
+            }
+        }
+
+        nDivider++;
+    }
+}
