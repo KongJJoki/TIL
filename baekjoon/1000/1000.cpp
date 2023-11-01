83d475db3ea4d02081a47648a3f87263f952f62e
KongJJoki <jeonga0208@gmail.com>
2023-11-01 17:01:07 +0900

    A+B

---
+++ baekjoon/1000/1000.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main()
+{
+    int nA{};
+    int nB{};
+    int nTotal{};
+
+    std::cin >> nA;
+    std::cin >> nB;
+
+    nTotal = nA + nB;
+
+    std::cout << nTotal;
+}
