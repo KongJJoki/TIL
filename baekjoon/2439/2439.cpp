03b1dc4faa1994fadaf730a8278dd12cc4bf6406
KongJJoki <jeonga0208@gmail.com>
2023-11-01 17:21:47 +0900

    별 찍기 - 2

---
+++ baekjoon/2439/2439.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main()
+{
+	int nCount{};
+	std::cin >> nCount;
+
+	for (int i = 0; i < nCount; i++)
+	{
+		int nTmp{};
+		nTmp = nCount - i - 1;
+		for (int j = 0; j < nTmp; j++)
+		{
+			std::cout << ' ';
+		}
+		for (int j = 0; j < i + 1; j++)
+		{
+			std::cout << '*';
+		}
+		std::cout << '\n';
+	}
+}
