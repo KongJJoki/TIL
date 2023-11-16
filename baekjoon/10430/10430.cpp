071bab3ae5bf96cad7e343ea461b0903378454fd
KongJJoki <jeonga0208@gmail.com>
2023-11-16 21:53:32 +0900

    나머지

---
+++ baekjoon/10430/10430.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main()
+{
+    int nInputA{};
+    int nInputB{};
+    int nInputC{};
+
+    std::cin >> nInputA;
+    std::cin >> nInputB;
+    std::cin >> nInputC;
+
+    std::cout << (nInputA + nInputB) % nInputC << '\n';
+    std::cout << ((nInputA % nInputC) + (nInputB % nInputC)) % nInputC << '\n';
+    std::cout << (nInputA * nInputB) % nInputC << '\n';
+    std::cout << ((nInputA % nInputC) * (nInputB % nInputC)) % nInputC;
+}
