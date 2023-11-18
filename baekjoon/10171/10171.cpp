d817b1329c1c991bdd06b7ff15b4b8e6d581716d
KongJJoki <jeonga0208@gmail.com>
2023-11-18 21:07:20 +0900

    고양이

---
+++ baekjoon/10171/10171.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::cout << "\\    /\\" <<'\n';
+    std::cout << " )  ( ')" << '\n';
+    std::cout << "(  /  )" << '\n';
+    std::cout << " \\(__)|";
+}
