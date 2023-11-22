434f9654d73470dae77be643ff50af670d32bd63
KongJJoki <jeonga0208@gmail.com>
2023-11-22 21:14:53 +0900

    단어 정렬

---
+++ baekjoon/1181/1181.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <algorithm>
+
+bool lenCompare(std::string str1, std::string str2);
+
+int main()
+{
+    int nLength{};
+    std::set<std::string> setStr{};
+
+    std::cin >> nLength;
+
+    for (int i = 0; i < nLength; i++)
+    {
+        std::string tmpStr{};
+        std::cin >> tmpStr;
+        setStr.insert(tmpStr);
+    }
+
+    std::vector<std::string> vecStr(setStr.begin(),setStr.end());
+    std::sort(vecStr.begin(), vecStr.end(), lenCompare);
+
+    nLength = vecStr.size();
+
+    for (int i = 0; i < nLength; i++)
+    {
+        std::cout << vecStr[i] << '\n';
+    }
+}
+
+bool lenCompare(std::string str1, std::string str2)
+{
+    if (str1.length() == str2.length())
+    {
+        return str1 < str2;
+    }
+    else
+    {
+        return str1.length() < str2.length();
+    }
+}
