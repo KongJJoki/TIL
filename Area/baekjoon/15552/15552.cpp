713e13301a3750e9ce7245eec148835fdd7a777a
KongJJoki <jeonga0208@gmail.com>
2023-12-03 17:37:00 +0900

    빠른 A+B

---
+++ Area/baekjoon/15552/15552.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    int nT{};
+    std::cin >> nT;
+
+    for (int i = 0; i < nT; i++)
+    {
+        int nA{};
+        int nB{};
+
+        std::cin >> nA;
+        std::cin >> nB;
+
+        std::cout << nA + nB << '\n';
+    }
+}
