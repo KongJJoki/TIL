99b75bce22f6cb559ff41cca69789de712c68a9a
KongJJok <jeonga0208@gmail.com>
2023-12-11 17:07:16 +0900

    A + B -8

---
+++ Area/baekjoon/11022/11022.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    int nT{};
+    std::cin >> nT;
+
+    for(int i = 0; i < nT; i++)
+    {
+        int nA{};
+        int nB{};
+
+        std::cin >> nA;
+        std::cin >> nB;
+
+        std::cout << "Case #" << i+1 << ": " << nA << " + " << nB << " = " << nA+nB << '\n';
+    }
+}
