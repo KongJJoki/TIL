d8bd44a4e69fecc99f3a32a8708fa33bf4374913
KongJJoki <jeonga0208@gmail.com>
2023-12-16 20:28:07 +0900

    회사에 있는 사람

---
+++ Area/baekjoon/7785/7785.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <map>
+#include <string>
+#include <algorithm>
+
+int main()
+{
+    int nN{};
+    int nCount{};
+    std::map<std::string, std::string, std::greater<std::string>> mapName{};
+
+    std::cin >> nN;
+
+    for (int i = 0; i < nN; i++)
+    {
+        std::string tmpName{};
+        std::string tmpEL{};
+
+        std::cin >> tmpName;
+        std::cin >> tmpEL;
+
+        if (tmpEL == "enter")
+        {
+            std::pair<std::string, std::string> tmpPair{};
+            tmpPair.first = tmpName;
+            tmpPair.second = tmpEL;
+
+            mapName.insert(tmpPair);
+
+            nCount++;
+        }
+        else if (tmpEL == "leave")
+        {
+            mapName.erase(tmpName);
+
+            nCount--;
+        }
+    }
+
+    std::map<std::string, std::string, std::greater<std::string>>::iterator iter;
+    for (iter = mapName.begin(); iter != mapName.end(); iter++)
+    {
+        std::cout << iter->first << '\n';
+    }
+}
