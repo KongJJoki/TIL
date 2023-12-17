719cc30b8507b762ae9d7c82902273bf441971ee
KongJJoki <jeonga0208@gmail.com>
2023-12-17 21:25:20 +0900

    A+B -4

---
+++ Area/baekjoon/10951/10951.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main()
+{
+    int nA{};
+    int nB{};
+
+    while (true)
+    {
+
+        std::cin >> nA;
+        std::cin >> nB;
+
+        if (std::cin.eof() == true)
+        {
+            break;
+        }
+
+        std::cout << nA + nB << '\n';
+    }
+}
