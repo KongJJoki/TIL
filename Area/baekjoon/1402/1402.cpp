1457f223cbe87f3da0786e17a15cb3e6431b334d
KongJJok <jeonga0208@gmail.com>
2023-12-19 20:04:38 +0900

    이문제는아무래도A난이도인것같다

---
+++ Area/baekjoon/1402/1402.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    int nT{};
+    std::cin >> nT;
+
+    for(int i = 0; i < nT; i++)
+    {
+        int nTmp1{};
+        int nTmp2{};
+        std::cin >> nTmp1;
+        std::cin >> nTmp2;
+
+        std::cout << "yes" << '\n';
+    }
+}
