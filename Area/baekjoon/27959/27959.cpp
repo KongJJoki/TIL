357aa7465d043f768dcf0853f6751b4a857c2347
KongJJoki <jeonga0208@gmail.com>
2023-12-29 12:01:42 +0900

    초코바

---
+++ Area/baekjoon/27959/27959.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main()
+{
+    int nN{};
+    int nM{};
+
+    std::cin >> nN;
+    std::cin >> nM;
+
+    nN *= 100;
+
+    if (nN >= nM)
+    {
+        std::cout << "Yes";
+    }
+    else
+    {
+        std::cout << "No";
+    }
+}
