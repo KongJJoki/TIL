52c1b02219de2f2d413eb9be4429ca0a47b2472f
KongJJoki <jeonga0208@gmail.com>
2023-12-31 13:13:10 +0900

    세수정렬

---
+++ Area/baekjoon/2752/2752.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <algorithm>
+
+int main()
+{
+    int nTmp{};
+    int nTmpMax{};
+    int arNum[3]{};
+
+    for (int i = 0; i < 3; i++)
+    {
+        std::cin >> arNum[i];
+    }
+    
+    std::sort(arNum, arNum + 3);
+
+    for (int i = 0; i < 3; i++)
+    {
+        std::cout << arNum[i] << ' ';
+    }
+}
