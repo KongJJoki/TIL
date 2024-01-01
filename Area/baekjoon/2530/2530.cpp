aac721fe0dfbcc3971d09da3dc2940137c144380
KongJJoki <jeonga0208@gmail.com>
2024-01-01 16:56:03 +0900

    인공지능 시계

---
+++ Area/baekjoon/2530/2530.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+int main()
+{
+	int nA{};
+	int nNewA{};
+	int nB{};
+	int nC{};
+	int nD{};
+
+	nNewA = -1;
+
+	std::cin >> nA;
+	std::cin >> nB;
+	std::cin >> nC;
+	std::cin >> nD;
+
+	nC += nD;
+
+	while (nC >= 60)
+	{
+		nC -= 60;
+		nB++;
+	}
+	while (nB >= 60)
+	{
+		nB -= 60;
+		nA++;
+	}
+	while (nA >= 24)
+	{
+		nA %= 24;
+	}
+
+	std::cout << nA << ' ' << nB << ' ' << nC;
+}
