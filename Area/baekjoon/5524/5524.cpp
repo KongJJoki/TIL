1352b659ef9bb665781591ccc20a77125c7de307
KongJJoki <jeonga0208@gmail.com>
2024-01-06 13:36:19 +0900

    입실 관리

---
+++ Area/baekjoon/5524/5524.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+int main()
+{
+    int nN{};
+    std::vector<std::string> vecName{};
+
+    std::cin >> nN;
+
+    for (int i = 0; i < nN; i++)
+    {
+        std::string strTmp{};
+        std::cin >> strTmp;
+        for (int j = 0; j < strTmp.size(); j++)
+        {
+            strTmp[j] = std::tolower(strTmp[j]);
+        }
+        vecName.push_back(strTmp);
+    }
+
+    for (int i = 0; i < vecName.size(); i++)
+    {
+        std::cout << vecName[i] << '\n';
+    }
+}
