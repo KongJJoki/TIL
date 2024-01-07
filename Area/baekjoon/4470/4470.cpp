a3fe6338688a717ccb19d30c502b711b4d1c493e
KongJJoki <jeonga0208@gmail.com>
2024-01-07 20:01:27 +0900

    줄번호

---
+++ Area/baekjoon/4470/4470.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+int main()
+{
+    int nN{};
+    std::vector<std::string> vecName{};
+    std::cin >> nN;
+    std::cin.ignore();
+
+    for (int i = 0; i < nN; i++)
+    {
+        std::string strTmp{};
+        std::getline(std::cin, strTmp);
+        vecName.push_back(strTmp);
+    }
+
+    for (int i = 0; i < nN; i++)
+    {
+        std::cout << i + 1 << ". " << vecName[i] << '\n';
+    }
+}
