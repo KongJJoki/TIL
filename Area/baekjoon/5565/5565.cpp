c836fbe1e4fe205d2beb773ef683e8c03898fb4d
KongJJoki <jeonga0208@gmail.com>
2024-01-17 23:34:01 +0900

    영수증

---
+++ Area/baekjoon/5565/5565.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main()
+{
+    int nTotal{};
+
+    std::cin >> nTotal;
+
+    for (int i = 0; i < 9; i++)
+    {
+        int nTmp{};
+        std::cin >> nTmp;
+        nTotal -= nTmp;
+    }
+
+    std::cout << nTotal;
+}
