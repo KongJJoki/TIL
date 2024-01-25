6f1e82fe7c03762d1e486adeaad40ab52e239fa0
KongJJoki <jeonga0208@gmail.com>
2024-01-25 20:56:34 +0900

    수들의 합

---
+++ Area/baekjoon/1789/1789.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int main()
+{
+    long long nS{};
+    int nResult{};
+    int nPivot{};
+    std::cin >> nS;
+    nPivot = 1;
+
+    while (nS > 0)
+    {
+        nS -= nPivot;
+
+        if (nS < 0)
+        {
+            break;
+        }
+        else
+        {
+            nPivot++;
+            nResult++;
+        }
+    }
+
+    std::cout << nResult;
+}
