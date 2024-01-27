5a4a4803b9d42dc9e81592872b49bac7be626853
KongJJoki <jeonga0208@gmail.com>
2024-01-27 10:52:38 +0900

    대표값2

---
+++ Area/baekjoon/2587/2587.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    std::vector<int> vecNumbers{};
+    int nAvg{};
+
+    for (int i = 0; i < 5; i++)
+    {
+        int nTmp{};
+        std::cin >> nTmp;
+        vecNumbers.push_back(nTmp);
+        nAvg += nTmp;
+    }
+    std::sort(vecNumbers.begin(), vecNumbers.end());
+
+    std::cout << nAvg / 5 << '\n';
+    std::cout << vecNumbers[2];
+}
