b9c7d6f9d76e165fe9f984b7f938d579eea770d6
KongJJoki <147111815+KongJJoki@users.noreply.github.com>
2024-01-29 18:50:04 +0900

    배열 합치기

---
+++ Area/baekjoon/11728/11728.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0);
+    std::cout.tie(0);
+
+    std::vector<int> vecNumbers{};
+
+    int nN{};
+    int nM{};
+
+    std::cin >> nN;
+    std::cin >> nM;
+
+    for (int i = 0; i < nN + nM; i++)
+    {
+        int nTmp{};
+        std::cin >> nTmp;
+        vecNumbers.push_back(nTmp);
+    }
+
+    std::sort(vecNumbers.begin(), vecNumbers.end());
+
+    for (int i = 0; i < nN + nM; i++)
+    {
+        std::cout << vecNumbers[i] << ' ';
+    }
+}
