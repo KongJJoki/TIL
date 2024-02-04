9a2326a34b936403f2d228d03893f6a3b5faaca1
KongJJoki <jeonga0208@gmail.com>
2024-02-04 21:47:58 +0900

    팬들에게 둘러싸인 홍준

---
+++ Area/baekjoon/14581/14581.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string strID{};
+
+    std::cin >> strID;
+
+    std::cout << ":fan::fan::fan:" << "\n";
+    std::cout << ":fan::"<< strID <<"::fan:" << "\n";
+    std::cout << ":fan::fan::fan:" << "\n";
+}
