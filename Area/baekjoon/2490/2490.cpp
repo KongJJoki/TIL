dbdea9f89299251d85bceaba3855595171fb8297
KongJJoki <147111815+KongJJoki@users.noreply.github.com>
2024-02-05 21:29:47 +0900

    윷놀이

---
+++ Area/baekjoon/2490/2490.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+int main()
+{
+    for (int i = 0; i < 3; i++)
+    {
+        int nZeroCount{};
+        for (int j = 0; j < 4; j++)
+        {
+            int nTmp{};
+            std::cin >> nTmp;
+            if (nTmp == 0)
+            {
+                nZeroCount++;
+            }
+        }
+
+        if (nZeroCount == 1)
+        {
+            std::cout << "A" << '\n';
+        }
+        else if (nZeroCount == 2)
+        {
+            std::cout << "B" << '\n';
+        }
+        else if (nZeroCount == 3)
+        {
+            std::cout << "C" << '\n';
+        }
+        else if (nZeroCount == 4)
+        {
+            std::cout << "D" << '\n';
+        }
+        else if (nZeroCount == 0)
+        {
+            std::cout << "E" << '\n';
+        }
+    }
+}
