41aabf64acfd36b7df2eca036490fea059de0388
KongJJoki <jeonga0208@gmail.com>
2024-02-14 22:21:39 +0900

    네 수

---
+++ Area/baekjoon/10824/10824.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string strA{};
+    std::string strB{};
+    std::string strC{};
+    std::string strD{};
+
+    long long nAB{};
+    long long nCD{};
+
+    std::cin >> strA;
+    std::cin >> strB;
+    std::cin >> strC;
+    std::cin >> strD;
+
+    nAB = std::stoll(strA + strB);
+    nCD = std::stoll(strC + strD);
+
+    std::cout << nAB + nCD;
+}
