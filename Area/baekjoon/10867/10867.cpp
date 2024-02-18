e32561085832eb7bd9ed0cccb8adb2e726c919d7
KongJJoki <jeonga0208@gmail.com>
2024-02-18 13:27:42 +0900

    중복 빼고 정렬하기

---
+++ Area/baekjoon/10867/10867.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    int nN{};
+    std::vector<int> vecNumbers{};
+    std::vector<int>::iterator iter{};
+
+    std::cin >> nN;
+    for (int i = 0; i < nN; i++)
+    {
+        int nTmp{};
+        std::cin >> nTmp;
+        vecNumbers.push_back(nTmp);
+    }
+
+    std::sort(vecNumbers.begin(), vecNumbers.end());
+    iter = std::unique(vecNumbers.begin(), vecNumbers.end());
+    vecNumbers.erase(iter, vecNumbers.end());
+
+    for (int i = 0; i < vecNumbers.size(); i++)
+    {
+        std::cout << vecNumbers[i] << ' ';
+    }
+}
