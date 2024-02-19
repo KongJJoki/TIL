32af969bf2db5e6f57b8fa40988b7fe010ad8ff9
KongJJoki <jeonga0208@gmail.com>
2024-02-19 17:42:31 +0900

    A+B - 6

---
+++ Area/baekjoon/10953/10953.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    int nT{};
+
+    std::cin >> nT;
+
+    for (int i = 0; i < nT; i++)
+    {
+        int nNum1{};
+        int nNum2{};
+        std::string strTmp{};
+        std::cin >> strTmp;
+
+        nNum1 = strTmp[0] - '0';
+        nNum2 = strTmp[2] - '0';
+        std::cout << nNum1 + nNum2 << '\n';
+    }
+}
