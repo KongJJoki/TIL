6f3a0c90c08a6a4f7aca4d2b9d498fa4ba1b6b17
KongJJoki <jeonga0208@gmail.com>
2024-02-20 12:09:24 +0900

    R2

---
+++ Area/baekjoon/3046/3046.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main()
+{
+    int nR1{};
+    int nR2{};
+    int nS{};
+
+    std::cin >> nR1;
+    std::cin >> nS;
+
+    nR2 = (nS * 2) - nR1;
+
+    std::cout << nR2;
+}
