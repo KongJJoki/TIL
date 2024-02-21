ae6afff65cf47620f15a06f91e8505c4b2290137
KongJJoki <jeonga0208@gmail.com>
2024-02-21 19:36:37 +0900

    베라의 패션

---
+++ Area/baekjoon/15439/15439.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main()
+{
+    int nN1{};
+    int nN2{};
+
+    std::cin >> nN1;
+
+    nN2 = nN1 - 1;
+    if (nN2 < 0)
+    {
+        nN2 = 0;
+    }
+
+    std::cout << nN1 * nN2;
+}
