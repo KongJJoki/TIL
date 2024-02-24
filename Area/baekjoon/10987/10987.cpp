2dad7e6cbeef9d44ab28facfae438bb5b02cbd52
KongJJoki <jeonga0208@gmail.com>
2024-02-24 18:42:18 +0900

    모음의 개수

---
+++ Area/baekjoon/10987/10987.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    int nResult{};
+    std::string strTest{};
+
+    std::cin >> strTest;
+    
+    for (int i = 0; i < strTest.length(); i++)
+    {
+        if (strTest[i] == 'a' || strTest[i] == 'e' || strTest[i] == 'i' || strTest[i] == 'o' || strTest[i] == 'u')
+        {
+            nResult++;
+        }
+    }
+
+    std::cout << nResult;
+}
