1bd03ba11f9e21726b4fe3ade85440b280197972
KongJJoki <jeonga0208@gmail.com>
2024-02-25 19:40:33 +0900

    Next in line

---
+++ Area/baekjoon/6749/6749.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+int main()
+{
+    int nYoung{};
+    int nSecond{};
+
+    std::cin >> nYoung;
+    std::cin >> nSecond;
+
+    std::cout << nSecond + (nSecond - nYoung);
+}
