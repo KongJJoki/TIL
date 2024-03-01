1e9c50c3a193f544789b36da6fc8668747f69726
KongJJoki <jeonga0208@gmail.com>
2024-03-01 16:35:22 +0900

    크냐?

---
+++ Area/baekjoon/4101/4101.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int main()
+{
+    while (true)
+    {
+        int nNum1{};
+        int nNum2{};
+
+        std::cin >> nNum1;
+        std::cin >> nNum2;
+
+        if (nNum1 == 0 && nNum2 == 0)
+        {
+            break;
+        }
+
+        if (nNum1 > nNum2)
+        {
+            std::cout << "Yes" << '\n';
+        }
+        else
+        {
+            std::cout << "No" << '\n';
+        }
+    }
+}
