79f7cd6cff1cd7d99dc70e41f2e2e54124e2eb3e
KongJJoki <jeonga0208@gmail.com>
2024-03-04 20:30:13 +0900

    10부제

---
+++ Area/baekjoon/10797/10797.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int main()
+{
+    int nDay{};
+    int nResult{};
+    int arrCar[5]{};
+
+    std::cin >> nDay;
+
+    for (int i = 0; i < 5; i++)
+    {
+        int nTmp{};
+        std::cin >> nTmp;
+        arrCar[i] = nTmp;
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        if (arrCar[i] % 10 == nDay)
+        {
+            nResult++;
+        }
+    }
+
+    std::cout << nResult;
+}
