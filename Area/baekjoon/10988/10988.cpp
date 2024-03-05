e4f9df1658187836062ccf260129e3b219f0c541
KongJJoki <jeonga0208@gmail.com>
2024-03-05 15:54:33 +0900

    팰린드롬인지 확인하기

---
+++ Area/baekjoon/10988/10988.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+int main()
+{
+    std::string strTest{};
+    std::string strTmp{};
+
+    std::cin >> strTest;
+
+    strTmp = strTest;
+    std::reverse(strTmp.begin(), strTmp.end());
+
+    if (strTest == strTmp)
+    {
+        std::cout << 1;
+    }
+    else
+    {
+        std::cout << 0;
+    }
+}
