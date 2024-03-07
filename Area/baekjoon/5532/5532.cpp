baea651b5beae3f7c57b7f9d75ad550cb99c9f15
KongJJoki <jeonga0208@gmail.com>
2024-03-07 22:34:38 +0900

    방학 숙제

---
+++ Area/baekjoon/5532/5532.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+int main()
+{
+    int nL{};
+    int nA{};
+    int nB{};
+    int nC{};
+    int nD{};
+    int nKorean{};
+    int nMath{};
+    int nBigger{};
+
+    std::cin >> nL;
+    std::cin >> nA;
+    std::cin >> nB;
+    std::cin >> nC;
+    std::cin >> nD;
+
+    if (nA % nC == 0)
+    {
+        nKorean = nA / nC;
+    }
+    else
+    {
+        nKorean = nA / nC + 1;
+    }
+
+    if (nB % nD == 0)
+    {
+        nMath = nB / nD;
+    }
+    else
+    {
+        nMath = nB / nD + 1;
+    }
+
+    if (nKorean > nMath)
+    {
+        nBigger = nKorean;
+    }
+    else
+    {
+        nBigger = nMath;
+    }
+
+    std::cout << nL - nBigger;
+}
