d79fee489c28f6e8bad2762e9146e71265e4e286
KongJJoki <jeonga0208@gmail.com>
2024-03-08 23:24:55 +0900

    상근이의 친구들

---
+++ Area/baekjoon/5717/5717.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main()
+{
+	int nM{};
+	int nF{};
+
+	while (true)
+	{
+		std::cin >> nM;
+		std::cin >> nF;
+		if (nM == 0 && nF == 0)
+		{
+			break;
+		}
+		std::cout << nM + nF << '\n';
+	}
+}
