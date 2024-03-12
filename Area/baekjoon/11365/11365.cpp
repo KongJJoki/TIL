9cd1806fe55f0261ca2b4e5e08fb55bf91f4dcef
KongJJoki <jeonga0208@gmail.com>
2024-03-12 23:11:10 +0900

    !밀비 급일

---
+++ Area/baekjoon/11365/11365.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+int main()
+{
+    std::string strTmp{};
+
+    while (true)
+    {
+        std::getline(std::cin, strTmp);
+        if (strTmp == "END")
+        {
+            break;
+        }
+
+        std::reverse(strTmp.begin(), strTmp.end());
+
+        std::cout << strTmp << '\n';
+    }
+}
